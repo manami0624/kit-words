fd3aabee0b74d7735b2376ec0f732407ba7bd587
manami0624 <manana624@yahoo.co.jp>
2023-07-09 20:38:55 +0900

    退会機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, :only => [:show, :destroy]
+
   def show
     @nickname = current_user.nickname
     @words = current_user.words.page(params[:page]).per(1)
     @total_posts = current_user.words.count
+  end
+
+  def destroy
+    @user = User.find(params[:id]) 
+    @user.destroy
+    flash[:notice] = 'ユーザーを削除しました。'
+    redirect_to root_path
+  end
+  private
+  def set_user
+    @user = User.find_by(:id => params[:id])
   end
 end
