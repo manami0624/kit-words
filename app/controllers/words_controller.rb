ccfe747d92657835efa8f8b5926bae8a856692c4
manami0624 <manana624@yahoo.co.jp>
2023-07-09 13:32:40 +0900

    MY単語帳ページ実装

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -10,13 +10,46 @@ class WordsController < ApplicationController
   end
 
   def create
-    Word.create(word_params)
+    @word = Word.new(word_params)
+    if @word.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    word = Word.find(params[:id])
+    word.destroy
+    redirect_to user_path(current_user)  
+  end
+
+  def edit
+    @word = Word.find(params[:id])
+  end
+
+  def update
+    @word = Word.find(params[:id])
+    if @word.update(word_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  def search
+    keyword = params[:keyword].to_s.strip
+    if keyword.empty?
+      redirect_to root_path
+    else
+      @words = Word.search(keyword)
+    end
   end
 
   private
 
   def word_params
-    params.require(:word).permit(:korean, :japanese)
+    params.require(:word).permit(:korean, :japanese).merge(user_id: current_user.id)
   end
 
   def move_to_index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'words#index'
-  resources :words, only: [:index, :new, :create]
+  resources :words do
+    collection do
+      get 'search'
+    end
+  end
+  resources :users, only: :show
 end
