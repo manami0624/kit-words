ccd84dec4bdfc1b1d9c67e215bc6fbc8b56a9e0e
manami0624 <manana624@yahoo.co.jp>
2023-07-09 12:00:31 +0900

    投稿機能実装

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,18 @@ class WordsController < ApplicationController
   def index
     @words = Word.all
   end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    Word.create(word_params)
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:korean, :japanese)
+  end
 end
