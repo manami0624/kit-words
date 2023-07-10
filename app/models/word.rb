c9a494ff096b552c3a9efce3c8c2ad5defdf9bc8
manami0624 <manana624@yahoo.co.jp>
2023-07-10 20:02:15 +0900

    投稿文字数制限追加

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,7 +2,7 @@ class Word < ApplicationRecord
   validates :korean, :japanese, presence: true
   belongs_to :user
 
-  validates :korean, :japanese, presence: true
+  validates :korean, :japanese, presence: true, length: { maximum: 10 }
   validates :korean, uniqueness: { scope: :user_id, message: "既に登録されている単語です" }
 
   def self.search(search)
