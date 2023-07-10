0ce194c71d101faf90b2e6adcee63c4e65549464
manami0624 <manana624@yahoo.co.jp>
2023-07-10 13:27:15 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :words, :dependent => :destroy
+
+  validates :nickname, presence: true, length: { maximum: 6 }
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'must include both letters and numbers' 
+
 end
