dfa05c43db93991012dbc66a87351836514f7025
manami0624 <manana624@yahoo.co.jp>
2023-07-11 13:38:21 +0900

    バリデーション設定

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,10 +1,9 @@
 class Word < ApplicationRecord
-  validates :korean, :japanese, presence: true
   belongs_to :user
 
-  validates :korean, :japanese, presence: true, length: { maximum: 10 }
-  validates :korean, uniqueness: { scope: :user_id, message: "既に登録されている単語です" }
-
+  validates :korean, presence: true, length: { maximum: 10 }, uniqueness: { scope: :user_id, message: "既に登録されている単語です" }
+  validates :japanese, presence: true, length: { maximum: 10 }
+  
   def self.search(search)
     if search != ""
       Word.where(['korean LIKE ? OR japanese LIKE ?', "%#{search}%", "%#{search}%"])
