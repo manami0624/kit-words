ecc9d5c6da3c915a4a83ba80f43c7208d9fe5fc4
manami0624 <manana624@yahoo.co.jp>
2023-07-15 16:02:47 +0900

    単語帳のソート機能追加

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,6 +4,10 @@ class Word < ApplicationRecord
   validates :korean, presence: true, length: { maximum: 10 }, uniqueness: { scope: :user_id, message: ": 既に登録されている単語です" }
   validates :japanese, presence: true, length: { maximum: 10 }
   
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :random, -> { order('RAND()') }
+  
   def self.search(search)
     if search != ""
       Word.where(['korean LIKE ? OR japanese LIKE ?', "%#{search}%", "%#{search}%"])
