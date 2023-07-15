7a9df05d97dd264d0e7a6558e39f71f595d4d556
manami0624 <manana624@yahoo.co.jp>
2023-07-15 22:23:14 +0900

    辞書機能の仕様変更

--- app/javascript/words.js
+++ app/javascript/words.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   contentPosts.forEach(function(post) {
     const koreanElement = post.querySelector('.korean-word');
     const japaneseElement = post.querySelector('.japanese-word');
+    const meaningElement = post.querySelector('.meaning');
 
     post.addEventListener('mouseover', function() {
       koreanElement.style.display = 'none';
@@ -13,6 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
     post.addEventListener('mouseout', function() {
       koreanElement.style.display = 'block';
       japaneseElement.style.display = 'none';
+    });
+
+    post.addEventListener('click', function() {
+      koreanElement.style.display = 'none';
+      japaneseElement.style.display = 'none';
+      meaningElement.style.display = 'block';
     });
   });
 });
--- db/migrate/20230710131205_create_dictionaries.rb
+++ db/migrate/20230710131205_create_dictionaries.rb
@@ -2,7 +2,8 @@ class CreateDictionaries < ActiveRecord::Migration[6.0]
   def change
     create_table :dictionaries do |t|
       t.string :vocabulary,   null: false
-      t.string :meaning,      null: false
+      t.string :japanese,     null: false
+      t.string :meaning
       t.integer :user_id
       t.timestamps
     end
