001b9927b501c7a4fe0db61c9058b730e66b7674
manami0624 <manana624@yahoo.co.jp>
2023-07-17 16:51:40 +0900

    dictionaryモデル単体テスト実施（修正後）

--- spec/models/dictionary_spec.rb
+++ spec/models/dictionary_spec.rb
@@ -5,40 +5,45 @@ RSpec.describe Dictionary, type: :model do
     @dictionary = FactoryBot.build(:dictionary)
   end
 
-  describe '辞書の保存' do
-    context '辞書が投稿できる場合' do
-      it '単語と意味を投稿できる' do
+  describe '単語帳の保存' do
+    context '単語帳が投稿できる場合' do
+      it '単語帳の表面と裏面とと意味を投稿できる' do
+        expect(@dictionary).to be_valid
+      end
+      it '意味は空でも投稿でる' do
+        @dictionary.meaning = ''
+        @dictionary.valid?
         expect(@dictionary).to be_valid
       end
       it '同じ単語でも他のユーザーであれば投稿できる' do
         @dictionary.save
         another_user = FactoryBot.create(:user)
-        another_dictionary = FactoryBot.build(:word, user_id: another_user.id, korean: @dictionary.vocabulary)
+        another_dictionary = FactoryBot.build(:dictionary, user_id: another_user.id, vocabulary: @dictionary.vocabulary)
         another_dictionary.valid?
         expect(another_dictionary).to be_valid
       end
     end
 
-    context '辞書が投稿できない場合' do
-      it '単語が空では投稿できない' do
+    context '単語帳が投稿できない場合' do
+      it '表面が空では投稿できない' do
         @dictionary.vocabulary = ''
         @dictionary.valid?
-        expect(@dictionary.errors.full_messages).to include("単語を入力してください")
+        expect(@dictionary.errors.full_messages).to include("単語帳の表面を入力してください")
       end 
-      it '意味が空では投稿できない' do
-        @dictionary.meaning = ''
-        @dictionary.valid?
-        expect(@dictionary.errors.full_messages).to include("意味を入力してください")
-      end
       it 'ユーザーが紐付いていなければ投稿できない' do
         @dictionary.user = nil
         @dictionary.valid?
         expect(@dictionary.errors.full_messages).to include('Userを入力してください')
       end
-      it '辞書が11文字以上だと投稿できない' do
+      it '表面が11文字以上だと投稿できない' do
         @dictionary.vocabulary = '테스트테스트테스트테스트'
         @dictionary.valid?
-        expect(@dictionary.errors.full_messages).to include("単語は10文字以内で入力してください")
+        expect(@dictionary.errors.full_messages).to include("単語帳の表面は10文字以内で入力してください")
+      end
+      it '裏面が11文字以上だと投稿できない' do
+        @dictionary.japanese = 'テストテストテストテスト'
+        @dictionary.valid?
+        expect(@dictionary.errors.full_messages).to include("単語帳の裏面は10文字以内で入力してください")
       end
       it '意味が121文字以上だと投稿できない' do
         @dictionary.meaning = '한' * 121
@@ -49,7 +54,7 @@ RSpec.describe Dictionary, type: :model do
         @dictionary.save
         another_dictionary = FactoryBot.build(:dictionary, user_id: @dictionary.user_id, vocabulary: @dictionary.vocabulary)
         another_dictionary.valid?
-        expect(another_dictionary.errors.full_messages).to include("単語: 既に登録されている単語です")
+        expect(another_dictionary.errors.full_messages).to include("単語帳の表面: 既に登録されている単語です")
       end
     end
   end
